6db9a12a9190443c45a2b7616674d8d8d5c8f717
Antoine Pirlot <pirlotantoine2@gmail.com>
2022-05-04 15:47:11 +0200

    modify server.c

--- server.c
+++ server.c
@@ -7,14 +7,25 @@
 #include "virement.h"
 
 #define BACKLOG 5
+#define MAX_VIREMENTS 10
+
+volatile sig_atomic_t end = false;
 
 int initSocket(int port);
 
 int main(int argc, char **argv) {
     int port = atoi(argv[1]);
-    int sockfd = initSocket(port);
+    int sockFd = initSocket(port);
+    Virement tabVirements[MAX_VIREMENTS], virement;
+    int nbVirements = 0;
     printf("Le serveur tourne sur le port: %d\n", port);
-    sclose(sockfd);
+    while(!end) {
+        int newSockFd = saccept(sockFd);
+        sread(newSockFd, &virement, sizeof(Virement));
+        printf("Virement pour : %d€", virement.somme);
+        nbVirements++;
+    }
+    sclose(sockFd);
     exit(0);
 }
 
