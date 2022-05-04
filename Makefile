218d76c6e80e5ad9919da76472578b07831f3255
LoicVinci <loic.hernaut@student.vinci.be>
2022-05-04 14:30:11 +0200

    Add pdr.c

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
 CCFLAGS=-D_DEFAULT_SOURCE -D_XOPEN_SOURCE -D_BSD_SOURCE -std=c11 -pedantic -Wvla -Wall -Werror
-execs=
+execs= pdr
 
 all:$(execs)
+
+pdr: pdr.c utils_v1.o
+	gcc $(CFLAGS) -o pdr pdr.c utils_v1.o
 
 clean:
 	rm *.o
---
+++ pdr.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "utils_v1.h"
+
+int main(int argc, char *argv[])
+{	
+	if(argc != 3) {
+		printf("Respectez la synthaxe de la fonction : pdr [compte] [montant]");
+		exit(0);
+	}
+	
+	int numCompte = atoi(argv[1]);
+	int montant = atoi(argv[2]);
+
+	printf("Num Compte : %d et Montant : %d", numCompte, montant);
+	return 0;
+}
