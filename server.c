2635c29fcd1241a632e01f92f9031a3e6cf2929f
Antoine Pirlot <pirlotantoine2@gmail.com>
2022-05-05 11:45:14 +0200

    Add poll

--- server.c
+++ server.c
@@ -5,6 +5,7 @@
 
 #include "utils_v1.h"
 #include "virement.h"
+#include "const.h"
 
 #define BACKLOG 5
 #define MAX_VIREMENTS 10
@@ -14,18 +15,40 @@ volatile sig_atomic_t end = false;
 int initSocket(int port);
 
 int main(int argc, char **argv) {
+    int nbVirements = 0, nbSockFd = 0;
     int port = atoi(argv[1]);
     int sockFd = initSocket(port);
     Virement tabVirements[MAX_VIREMENTS], virement;
-    int nbVirements = 0;
+    struct pollfd fds[1024];
+    bool fds_invalid[1024];
     printf("Le serveur tourne sur le port: %d\n", port);
-    while(!end) {
-        int newSockFd = saccept(sockFd);
-        sread(newSockFd, &virement, sizeof(Virement));
-        printf("Virement pour : %d€", virement.somme);
-        nbVirements++;
+
+    fds[nbSockFd].fd = sockFd;
+    fds[nbSockFd].events = POLLIN;
+    fds_invalid[nbSockFd] = false;
+    nbSockFd++;
+
+    while (!end) {
+        spoll(fds, nbSockFd, 0);
+        if (fds[0].revents & POLLIN & !fds_invalid[0]) {
+            int newSockFd = saccept(sockFd);
+            fds[nbSockFd].fd = newSockFd;
+            fds[nbSockFd].events = POLLIN;
+            nbSockFd++;
+            fds_invalid[nbSockFd] = false;
+        }
+        for (int i = 1; i < nbSockFd; i++) {
+            if (fds[i].revents & POLLIN && !fds_invalid[i]) {
+                sread(fds[i].fd, &virement, sizeof(virement));
+                nwrite(fds[i].fd, &virement, sizeof(virement));
+                printf("Virement pour : %d€\n", virement.somme);
+                nbVirements++;
+            }
+            //sleep(2);
+            sclose(fds[i].fd);
+            fds_invalid[i] = true;
+        }
     }
-    sclose(sockFd);
     exit(0);
 }
 
